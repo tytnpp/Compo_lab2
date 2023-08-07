44a490e45497605c98c4cc2897e440f8f1ca0dca
tytnpp <thanyatorn_n@cmu.ac.th>
2023-08-07 23:22:27 +0700

    push to github : Compo-Lab2

---
+++ real-world-vue/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import StudentView from '../views/StudentView.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'EventList',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: AboutView
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: CategoryView
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentView
+    }
+  ]
+})
+
+export default router
---
+++ real-world-vue/src/views/EventListView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import EventCard from '../components/EventCard.vue'
+import type { EventItem } from '@/type'
+
+import { ref } from 'vue'
+import EventService from '@/services/EventService'
+const events = ref<EventItem[]>([])
+EventService.getEvent().then((response) => {
+  events.value = response.data
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <main class="events">
+    <EventCard v-for="event in events" :key="'event.id'" :event="event"></EventCard>
+  </main>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
